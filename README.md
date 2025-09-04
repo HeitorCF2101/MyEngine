9c8007777d96c53d24239829173e63be1871dc1c
HeitorCF2101 <heitorcafr@gmail.com>
2025-09-04 11:39:20 -0300

    Criação de um sistema de combate básico por texto

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # MyEngine
-This repository is beeing used for study and aplication of concepts that I'm learning by myself
+This repository is beeing used for study and aplication of concepts that I'm learning by myself, is meant to be the folder of the final product of my work and an asset for job interviews and my Linkedin.
--- main.cpp
+++ main.cpp
@@ -3,19 +3,28 @@
 
 using namespace std;
 
+// Character structure to hold character properties and methods
 struct Character{
     string name;
     string sprite;
     int health;
     int damage;
 
+    // Method to render character with its current health
     void renderCharacter(){
         cout << name << "\t" << "Health: " << health << "\n";
         cout << sprite << "\n";
     }
 
+    // Method for attacking another character
+    void attack(Character &target){
+        cout << name << "attacks " << target.name << "and deals " << damage << "damage!\n";
+        target.health -= damage;
+    }
+
 };
 
+// Function to initialize a character
 void initCharacter(Character &character, string name, string sprite, int health, int damage){
     character.name = name;
     character.sprite = sprite;
@@ -23,14 +32,45 @@ void initCharacter(Character &character, string name, string sprite, int health,
     character.damage = damage;
 }
 
+// Function to update and render all characters int the scene
+void update(Character &character[]){
+    if(character[0].health > 0 && character[1].health > 0){
+        character[0].renderCharacter();
+        cout << "\n";
+        character[1].renderCharacter();
+    }
+}
+
 int main(){
+    // create characters
     Character hero, enemy;
     initCharacter(hero, "Hero", "(._.)", 100, 10);
     initCharacter(enemy, "Enemey", "(.o.)", 50, 5);
 
+    Character characters[] = {hero, enemy};
+
+    // Initial render
+    cout << "Hero vs Enemy\n";
     hero.renderCharacter();
     cout << "\n";
     enemy.renderCharacter();
+
+    // Combat loop
+    while(enemy.health > 0 && hero.health > 0){
+        hero.attack(enemy);
+        if(enemy.health <=0){
+            cout << enemy.name << " has been defeated!\n";
+            break;
+        }
+        enemy.attack(hero);
+        if(hero.health <= 0){
+            cout << hero.name << " has been defeated!\n";
+            break;
+        }
+        system("cls"); // Using "cls" for windows, for Linux use "clear"
+        cout << "\n";
+        update(characters);
+    }
     
     return 0;
 }
