70bd000bb9055449433e63cbd783352d0314b36d
HeitorCaFr <heitorcafr@gmail.com>
2026-03-05 11:53:27 +0000

    changing the project to use multiple files

--- source/main.cpp
+++ source/main.cpp
@@ -2,6 +2,7 @@
 #include <string>
 #include <thread>
 #include <limits>
+#include "character.cpp"
 
 using namespace std;
 
@@ -34,57 +35,6 @@ void initCharacter(Character &character, string name, string sprite, int health,
     character.health = health;
     character.damage = damage;
 }*/
-
-// Character class to hold character properties and methods
-class Character{
-    public:
-        string name;
-        string sprite;
-        int health;
-
-        // Method to initialize a character
-        void init(const string &characterName, const string &characterSprite, int characterHealth){
-            name = characterName;
-            sprite = characterSprite;
-            health = characterHealth;
-            
-        }
-
-        // Method to render character with its current health
-        void renderCharacter(){
-            cout << name << "\t" << "Health: " << health << "\n";
-            cout << sprite << "\n";
-        }
-
-        // Method to check if the character is alive
-        bool isAlive() const {
-            return health > 0;
-        }
-
-};
-
-class Attack{
-    public:
-        Character* attacker;
-        string name;
-        int damage;
-        string type;
-
-        // Method to initialize an attack
-        void init(Character* attacker, const string &attackName, int attackDamage, const string &attackType){
-            this->attacker = attacker;
-            name = attackName;
-            damage = attackDamage;
-            type = attackType;
-        }
-
-        // Method to execute the attack on a target character
-        void attackTarget(Character &target){
-            cout << "\n" << name << " uses " << name << "!\n" << "and deals " << damage << " " << type << " damage to " << target.name << "!\n";
-            target.health -= damage;
-        }
-
-};
 
 class Map{
     public:
---
+++ source/renderer.cpp
@@ -0,0 +1,8 @@
+/*
+    In the present moment, my renderer will be using OpenGL under the SFML library managment
+*/
+
+#include <iostream>
+#include <string>
+
+using namespace std;
